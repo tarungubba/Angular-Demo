47b6b80205a3e879e3256798ec7df5a27950cffb
tarungubba <tarun.399@gmail.com>
2016-10-11 21:35:21 +0530

    commit routes

--- angular-quickstart/app/hero-detail.component.ts
+++ angular-quickstart/app/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+import { Hero }                     from './hero';
+import { HeroService }              from './hero.service';
+
 @Component({
   selector: 'my-hero-detail',
   template: `
@@ -10,10 +15,28 @@ import { Hero } from './hero';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+	  <button (click)="goBack()">Back</button>
     </div>
   `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+	constructor(
+  private heroService: HeroService,
+  private route: ActivatedRoute,
+  private location: Location
+) {}
+
+ngOnInit(): void {
+  this.route.params.forEach((params: Params) => {
+    let id = +params['id'];
+    this.heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  });
+}
+goBack(): void {
+  this.location.back();
+}
+
   @Input()
   hero: Hero;
 }
